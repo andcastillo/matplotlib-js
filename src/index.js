ab98b1ec0089dbe8001e32f01bda7b7296c7f558
andres castillo <andcastillo@gmail.com>
2019-08-12 19:32:35 -0500

    fix username

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ const hist = require('./hist');
 const scatterplot = require('./scatterplot');
 const violinplot = require('./violinplot');
 const pie = require('./pie');
+const tree = require('./tree')
 
-module.exports = {hist, scatterplot, violinplot, pie};
+module.exports = {hist, scatterplot, violinplot, pie, tree};
---
+++ src/tree.js
@@ -0,0 +1,13 @@
+const colorScale = require('./colorScale');
+
+/**
+ * Create a tree from the dataframe 
+ * @param {object} data 
+ * @param {object} params x, y, color, palette, makerStyle, size, xlab, ylab, highlight
+ * @example tree(data, {x: "Sepal.Length", y: "Sepal.Width", color: "Species", palette: ["red", "green", "blue"]} );
+ */
+let tree = function(data, params) {
+
+}
+
+module.exports = tree;
